ed0f9a0c713c09905b38887ae2a23c05406d2f2b
cashalotkzn <cbvgcjys95>
2020-10-11 03:14:40 +0300

    init

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -17,6 +17,6 @@ class Controller extends BaseController
     public function index() {
         $news = News::latest()->where('published', 1)->take(5)->get();
         $streams = Stream::orderBy('updated_at', 'desc')->take(19)->get()->sortByDesc('viewers');
-        return view('Home', compact('news', 'streams'));
+        return view('home', compact('news', 'streams'));
     }
 }
