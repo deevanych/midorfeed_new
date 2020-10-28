aca3b63b1d326c0ebaf0f49038ddecf8683cca7b
cashalotkzn <cashalotkzn@gmail.com>
2020-10-28 07:08:02 +0300

    Дордобэ

--- resources/views/teammates/index.blade.php
+++ resources/views/teammates/index.blade.php
@@ -8,8 +8,19 @@
         <div class="col-7">
             @foreach($orders as $order)
                 <div class="row">
-                    <div class="col find-order__item" style="background-image: url("{{ asset(Storage::url('avatars/'.$order->user->steamid.'.jpg'))  }}")">
-                        {{ $order->user->personaname }}
+                    <div class="col">
+                        <div class="find-order__item">
+                            <div class="background fill blur"
+                                 style="background-image: url({{ asset(Storage::url('avatars/'.$order->user->steamid.'.jpg')) }})"></div>
+                            <div class="background-dark fill"></div>
+                            <div class="find-order__item-body">
+                                <div class="find-order__item-image"
+                                     style="background-image: url({{ asset(Storage::url('avatars/'.$order->user->steamid.'.jpg')) }})"></div>
+                                <div class="find-order__item-text">
+                                    <h2>{{ $order->user->personaname }}</h2>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             @endforeach
